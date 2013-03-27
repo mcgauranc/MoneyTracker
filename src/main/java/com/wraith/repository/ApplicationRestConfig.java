d4af185a1e8037d983f6c171c89e12f89a827eb8
ruaghain <ruaghain_dev@hotmail.com>
2013-03-27 22:41:31 +0000

    Changed the code to allow date fields to be displayed in ISO8601 format. Enabled some searching options on the user repository.

--- src/main/java/com/wraith/repository/ApplicationRestConfig.java
+++ src/main/java/com/wraith/repository/ApplicationRestConfig.java
@@ -1,19 +1,10 @@
 package com.wraith.repository;
 
-import com.wraith.json.ISO8601DateFormatModule;
-import com.wraith.repository.entity.Users;
-import org.springframework.context.ApplicationListener;
-import org.springframework.context.annotation.Bean;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.util.ISO8601DateFormat;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
-import org.springframework.data.rest.repository.context.AfterSaveEvent;
 import org.springframework.data.rest.webmvc.config.RepositoryRestMvcConfiguration;
-import org.springframework.util.ClassUtils;
-import org.springframework.validation.Errors;
-import org.springframework.validation.Validator;
-
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * User: rowan.massey
@@ -21,71 +12,13 @@ import java.util.List;
  * Time: 20:07
  */
 @Configuration
-@Import(RepositoryRestMvcConfiguration.class)
-public class ApplicationRestConfig {
+//@Import(RepositoryRestMvcConfiguration.class)
+public class ApplicationRestConfig extends RepositoryRestMvcConfiguration {
 
-//    @Bean
-//    public ResourceProcessor<Resource<Users>> userProcessor() {
-//        return new ResourceProcessor<Resource<Users>>() {
-//            @Override
-//            public Resource<Users> process(Resource<Users> resource) {
-//                resource.add(new Link("http://localhost:8080/testlink", "test.link"));
-//                return resource;
-//            }
-//        };
-//    }
+    @Override
+    protected void configureJacksonObjectMapper(ObjectMapper objectMapper) {
+        objectMapper.configure(SerializationFeature.WRITE_DATE_KEYS_AS_TIMESTAMPS, false);
+        objectMapper.setDateFormat(new ISO8601DateFormat());
 
-    @Bean
-    public Validator userValidator() {
-        return new Validator() {
-
-            List<String> badWords = new ArrayList<>();
-
-            {
-                badWords.add("fuck");
-            }
-
-            @Override
-            public boolean supports(Class<?> clazz) {
-                return ClassUtils.isAssignable(clazz, Users.class);
-            }
-
-            @Override
-            public void validate(Object target, Errors errors) {
-                Users user = (Users) target;
-
-                if (user.getUserName() != null) {
-                    return;
-                }
-
-                for (String words : badWords) {
-                    if (user.getUserName().toLowerCase().contains(words)) {
-                        errors.rejectValue("User name", "Invalid user name.");
-                    }
-                }
-            }
-        };
-    }
-
-//    @Bean
-//    public AnnotatedHandlerRepositoryEventListener handlerListener() {
-//        return new AnnotatedHandlerRepositoryEventListener("com.wraith.repository.handlers");
-//    }
-
-    @Bean
-    public ApplicationListener<AfterSaveEvent> afterSaveHandler() {
-        return new ApplicationListener<AfterSaveEvent>() {
-            @Override
-            public void onApplicationEvent(AfterSaveEvent event) {
-                if (event.getSource() instanceof Users) {
-                    System.out.println("Got a user " + event.getSource());
-                }
-            }
-        };
-    }
-
-    @Bean
-    public ISO8601DateFormatModule dateFormatModule() {
-        return new ISO8601DateFormatModule();
     }
 }
--- src/main/java/com/wraith/repository/UsersRepository.java
+++ src/main/java/com/wraith/repository/UsersRepository.java
@@ -1,10 +1,16 @@
 package com.wraith.repository;
 
 import com.wraith.repository.entity.Users;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.convert.ISO8601DateConverter;
+import org.springframework.data.rest.repository.annotation.ConvertWith;
 import org.springframework.data.rest.repository.annotation.RestResource;
 
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -12,14 +18,18 @@ import java.util.List;
  * Date: 24/01/13
  * Time: 22:01
  */
-@RestResource(path = "user")
+@RestResource(rel = "user", path = "user")
 public interface UsersRepository extends PagingAndSortingRepository<Users, Integer> {
 
     @RestResource(path = "username", rel = "username")
     public List<Users> findByUserName(@Param("username") String username);
 
-    //@RestResource(rel = "enabled")
-//    public Page<User> findByEnabledIsTrue(Pageable pageable);
-//
-//    public List<User> findByCreatedAfter(@Param("when") @ConvertWith(ISO8601StringToDateConverter.class) Date when);
+    @RestResource(rel = "enabled")
+    public Page<Users> findByEnabledIsTrue(Pageable pageable);
+
+    @RestResource(rel = "disabled")
+    public Page<Users> findByEnabledIsFalse(Pageable pageable);
+
+    @Query("select u from Users u where u.createdDate > :date")
+    public Page<Users> findByCreatedAfter(@Param("date") @ConvertWith(ISO8601DateConverter.class) Date date, Pageable pageable);
 }
