4e4d91af17e91c2b13b54230f37f8937c92ad2e4
Rowan Massey <ruaghain_dev@hotmail.com>
2014-01-30 13:42:09 +0000

    Implemented a PUT method of the user. Amended the grid to allow for inserts - need to implement the functionality for this though.

--- MoneyTrackerClient/web/app/js/services.js
+++ MoneyTrackerClient/web/app/js/services.js
@@ -5,9 +5,20 @@
 var userService = angular.module("userService", ["ngResource"]);
 var accountService = angular.module("accountService", ["ngResource"]);
 
+//userService.factory("User", ["$resource",
+//    function ($resource) {
+//        return $resource("/server/userss/:userId", {}, {
+//            query: {
+//                method: "GET", params: {
+//                    userId: ""},
+//                isArray: false}
+//        });
+//    }]);
+
 userService.factory("User", ["$resource",
     function ($resource) {
         return $resource("/server/userss/:userId", {}, {
-            query: {method: "GET", params: {userId: ""}, isArray: false}
+            update: {
+                method: "PUT" }
         });
     }]);
