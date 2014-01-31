98c251c1a043e67a4e7aea0d18ecc84c4699299c
Ruaghain <ruaghain_dev@hotmail.com>
2014-01-31 18:24:52 +0000

    Implemented a PUT method of the user. Amended the grid to allow for inserts - need to implement the functionality for this though.

--- MoneyTrackerClient/web/app/js/controllers.js
+++ MoneyTrackerClient/web/app/js/controllers.js
@@ -7,8 +7,20 @@ var accountController = angular.module("accountController", []);
 
 userController.controller("UserListCtrl", ["$scope", "User",
     function ($scope, User) {
-        $scope.users = User.query();
+        $scope.userData = {
+            dataSource: {
+                transport: {
+                    read: User.query(),
+//                    update: User.update(),
+                    create: User.save()
+                }
+            }
+        }
     }]);
+
+//    function ($scope, User) {
+//        $scope.users = User.query();
+//    }]);
 
 userController.controller("UserDetailCtrl", ["$scope", "$routeParams", "User",
     function ($scope, $routeParams, User) {
--- MoneyTrackerClient/web/app/js/services.js
+++ MoneyTrackerClient/web/app/js/services.js
@@ -5,20 +5,12 @@
 var userService = angular.module("userService", ["ngResource"]);
 var accountService = angular.module("accountService", ["ngResource"]);
 
-//userService.factory("User", ["$resource",
-//    function ($resource) {
-//        return $resource("/server/userss/:userId", {}, {
-//            query: {
-//                method: "GET", params: {
-//                    userId: ""},
-//                isArray: false}
-//        });
-//    }]);
-
 userService.factory("User", ["$resource",
     function ($resource) {
-        return $resource("/server/userss/:userId", {}, {
+        return $resource("/server/userss/", {
+            query: {
+                method: 'GET', isArray: false},
             update: {
-                method: "PUT" }
+                method: "PUT"}
         });
     }]);
