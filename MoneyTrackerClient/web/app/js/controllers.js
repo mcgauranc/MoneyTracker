62d4ce41010cca244f54886ab26ffefe0031ed5c
ruaghain <ruaghain_dev@hotmail.com>
2014-02-21 22:30:03 +0000

    Changed the code to correctly retrieve the required JSON data from the server, and display it on the kendo grid - using angular.

--- MoneyTrackerClient/web/app/js/controllers.js
+++ MoneyTrackerClient/web/app/js/controllers.js
@@ -8,53 +8,48 @@ var accountController = angular.module("accountController", []);
 userController.controller("UserListCtrl", ['$scope', 'Users',
     function ($scope, Users) {
 
-        $scope.userList = Users.query();
-        $scope.userList.$promise.then(function (data) {
-            console.log(data)
+        var userList = Users.query();
+        userList.$promise.then(function (data) {
+            populateDataSource(data)
+        }, function (status) {
+            console.log(status)
         });
 
-        $scope.usersDataSource = new kendo.data.DataSource({
-            dataSource: {
-                data: $scope.userList,
-                pageSize: 20
-//                serverPaging: true,
-//                transport: {
-//                    read: Users.query()
-//                    update: User.update(),
-//                    create: User.save()
-//                }
-            },
-            schema: {
-                data: "content",
-//                data: function(response) {
-//                    console.log("The content of the response is: " + response.content);
-//                    return response.content;
-//                },
-                model: {
-                    fields: {
-                        userName: {
-                            editable: true,
-                            nullable: false
-                        },
-                        firstName: {
-                            editable: true,
-                            nullable: false
-                        },
-                        lastName: {
-                            editable: true,
-                            nullable: false
-                        },
-                        enabled: {
-                            type: "boolean",
-                            editable: true,
-                            nullable: false
+        $scope.users = {};
+        $scope.users.dataSource = new kendo.data.DataSource();
+
+        var populateDataSource = function (data) {
+            var dataSource = new kendo.data.DataSource({
+                data: data,
+                pageSize: 20,
+                schema: {
+                    data: "content",
+                    model: {
+                        fields: {
+                            userName: {
+                                editable: true,
+                                nullable: false
+                            },
+                            firstName: {
+                                editable: true,
+                                nullable: false
+                            },
+                            lastName: {
+                                editable: true,
+                                nullable: false
+                            },
+                            enabled: {
+                                type: "boolean",
+                                editable: true,
+                                nullable: false
+                            }
                         }
                     }
                 }
-//                ,
-//                total: "totalPages"
-            }
-        })
+            });
+            dataSource.read();
+            $scope.users.dataSource = dataSource;
+        }
     }]);
 
 userController.controller("UserDetailCtrl", ["$scope", "$routeParams", "User",
