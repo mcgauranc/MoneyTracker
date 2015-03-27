e76dec89cd4f6ff1b76ee44df05b49e5206ce9db
Rowan Massey <ruaghain_dev@hotmail.com>
2015-03-27 15:50:07 +0000

    Finally got the grunt tasks working correctly. Running "grunt dev" set up a local server (with proxy) to make calls to the started server on a different port.  All tests are executed when code changes are made, this along with concatination and  jshint

--- Gruntfile.js
+++ Gruntfile.js
@@ -18,11 +18,8 @@ module.exports = function (grunt) {
                 configFile: 'karma.conf.js'
             },
             unit: {
-                singleRun: true
-            },
-            continuous: {
-                singleRun: false,
-                autoWatch: false
+                background: true,
+                singleRun: false
             }
         },
         wiredep: {
@@ -83,8 +80,10 @@ module.exports = function (grunt) {
                 "web/src/main/resources/static/components/**/*.js",
                 "web/src/main/resources/static/functionality/**/*.js",
                 "web/src/main/resources/static/components/**/*.css",
-                "web/src/main/resources/static/functionality/**/*.css"],
-            tasks: ["jshint", "karma", "concat"]
+                "web/src/main/resources/static/functionality/**/*.css",
+                "!web/src/main/resources/static/MoneyTracker.js",
+                "!web/src/main/resources/static/MoneyTracker.css"],
+            tasks: ["jshint", "concat"]
         },
         connect: {
             server: {
@@ -92,24 +91,40 @@ module.exports = function (grunt) {
                     hostname: 'localhost',
                     port: 9000,
                     base: 'web/src/main/resources/static/',
-                    keepalive: true
+                    livereload: true,
+                    open: true,
+                    middleware: function (connect, options) {
+                        if (!Array.isArray(options.base)) {
+                            options.base = [options.base];
+                        }
+
+                        // Setup the proxy
+                        var middlewares = [require('grunt-connect-proxy/lib/utils').proxyRequest];
+
+                        // Serve static files.
+                        options.base.forEach(function (base) {
+                            middlewares.push(connect.static(base));
+                        });
+
+                        // Make directory browse-able.
+                        var directory = options.directory || options.base[options.base.length - 1];
+                        middlewares.push(connect.directory(directory));
+
+                        return middlewares;
+                    }
                 },
                 proxies: [
                     {
-                        context: '/api/',
+                        context: '/api',
                         host: 'localhost',
                         port: 8080
-                        //https: false,
-                        //xforward: false
-                        //headers: {
-                        //    "x-custom-added-header": value
-                        //}
                     }
                 ]
             }
         }
     });
 
-    grunt.registerTask("dev", ["concat", "connect", "watch"]);
+    grunt.registerTask("server", ["configureProxies:server", "connect"]);
+    grunt.registerTask("dev", ["concat", "karma", "server", "watch"]);
     grunt.registerTask("prod", ["wiredep", "jshint", "concat", "uglify"])
 };
