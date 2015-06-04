3e120f0b45bcdbd2b9ea710dc10f4de3a1d1b3b9
Rowan Massey <ruaghain_dev@hotmail.com>
2015-06-04 21:16:00 +0100

    Finally managed to get the lookups working like I want them. Need to work on allowing the user to select a value, and it being populated in the edit.

--- web/src/main/resources/static/components/lookup/lookup-directive.js
+++ web/src/main/resources/static/components/lookup/lookup-directive.js
@@ -13,34 +13,21 @@
             restrict: "E",
             replace: true,
             scope: {
-                searchLabel: "@",
-                searchFunction: "&"
+                lookupLabel: "@",
+                lookupFunction: "&",
+                lookupDisplay: "="
             },
             templateUrl: "components/lookup/lookup.html",
             controller: function ($scope, $q) {
                 $scope.foundRecords = [];
                 $scope.searchTerm = "";
-                $scope.displayField = [{searchField: 'firstName'}, {searchField: 'lastName'}];
-
-                $scope.displays = processDisplayFields($scope.displayField);
-
                 $scope.search = function () {
                     var defer = $q.defer();
-                    defer.resolve($scope.searchFunction({searchValue: $scope.searchTerm}));
+                    defer.resolve($scope.lookupFunction({searchValue: $scope.searchTerm}));
                     defer.promise.then(function (searchResults) {
                         $scope.foundRecords = searchResults;
                     });
                 };
-
-                function processDisplayFields(displayFields) {
-                    var show = "";
-                    for (var i in displayFields) {
-                        show += "item." + displayFields[i].searchField + " ";
-                    }
-                    return show;
-                }
-            },
-            link: function ($scope) {
             }
         };
     };
