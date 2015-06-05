2cfa4093a377c823e0b3fd2bfc9c5db0e447588c
Rowan Massey <ruaghain_dev@hotmail.com>
2015-06-05 12:09:22 +0100

    Changed the unit tests to contain the required attributes for the lookup component.

--- web/src/main/resources/static/components/lookup/lookup-directive-test.js
+++ web/src/main/resources/static/components/lookup/lookup-directive-test.js
@@ -11,13 +11,12 @@ describe("Directive: mnyLookup", function () {
         $rootScope = _$rootScope_;
         $timeout = _$timeout_;
 
-        element = angular.element("<mny-lookup ></mny-lookup>");
+        element = angular.element("<mny-lookup lookup-label='User' lookup-function='findUsers()' lookup-display='['firstName']' ></mny-lookup>");
         $compile(element)($rootScope.$new());
     }));
 
     it("Replaces the element with the appropriate content for lookups", function () {
         $rootScope.$digest();
-
         expect(element.html()).toContain('');
     });
 
