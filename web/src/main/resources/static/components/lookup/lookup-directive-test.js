b7a70bd8a7b5de556f424717ec6f77924aaa3805
Rowan Massey <ruaghain_dev@hotmail.com>
2015-06-07 21:55:16 +0100

    Added in test scenarios for the lookup component.

--- web/src/main/resources/static/components/lookup/lookup-directive-test.js
+++ web/src/main/resources/static/components/lookup/lookup-directive-test.js
@@ -2,22 +2,48 @@ describe("Directive: mnyLookup", function () {
     var $compile,
         $rootScope,
         $timeout,
+        $httpBackend,
+        $q,
         element;
 
     beforeEach(module("moneyApp"));
 
-    beforeEach(inject(function (_$rootScope_, _$compile_, _$timeout_) {
+    beforeEach(module("templates"));
+
+    beforeEach(inject(function (_$rootScope_, _$compile_, _$timeout_, _$httpBackend_, _$q_) {
         $compile = _$compile_;
         $rootScope = _$rootScope_;
         $timeout = _$timeout_;
+        $httpBackend = _$httpBackend_;
+        $q = _$q_;
 
-        element = angular.element("<mny-lookup lookup-label='User' lookup-function='findUsers()' lookup-display='['firstName']' ></mny-lookup>");
+        element = angular.element('<mny-lookup lookup-label="User" lookup-function="findUsers()" lookup-display="[\'firstName\']"></mny-lookup>');
         $compile(element)($rootScope.$new());
     }));
 
-    it("Replaces the element with the appropriate content for lookups", function () {
+    it("Replaces the element with the appropriate text for lookups", function () {
         $rootScope.$digest();
-        expect(element.html()).toContain('');
+        expect(element.find("input").attr("type")).toEqual('text');
+    });
+
+    it("Contains the relevant model attribute", function () {
+        $rootScope.$digest();
+        expect(element.find("input").attr("ng-model")).toEqual('searchTerm');
+    });
+
+    it("Contains the relevant keyup attribute", function () {
+        $rootScope.$digest();
+        expect(element.find("input").attr("ng-keyup")).toEqual('search()');
+    });
+
+    it("Contains the relevant angular class attribute values", function () {
+        $rootScope.$digest();
+        expect(element.find("input").attr("class")).toEqual('ng-pristine ng-untouched ng-valid');
+    });
+
+    it("Performs search on key up", function () {
+        $rootScope.$digest();
+        element.find("input").triggerHandler("keyup");
     });
 
 });
