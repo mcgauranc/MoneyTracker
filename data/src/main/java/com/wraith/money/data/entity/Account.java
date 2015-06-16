83c7d766af4be8eb164af9547bf452a0da0246d3
Rowan Massey <ruaghain_dev@hotmail.com>
2015-06-16 16:47:12 +0100

    Added in unit tests for the account service.

--- data/src/main/java/com/wraith/money/data/entity/Account.java
+++ data/src/main/java/com/wraith/money/data/entity/Account.java
@@ -1,6 +1,17 @@
 package com.wraith.money.data.entity;
 
-import com.wraith.money.data.BaseEntity;
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.AttributeOverride;
+import javax.persistence.AttributeOverrides;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
 import org.hibernate.annotations.Cache;
 import org.hibernate.annotations.CacheConcurrencyStrategy;
 import org.hibernate.annotations.NaturalId;
@@ -8,111 +19,106 @@ import org.hibernate.envers.AuditTable;
 import org.hibernate.envers.Audited;
 import org.hibernate.envers.NotAudited;
 
-import javax.persistence.*;
-import java.io.Serializable;
-import java.util.Date;
+import com.wraith.money.data.BaseEntity;
 
 /**
  * This class is the object representation of an account in the database.
  * <p/>
- * User: rowan.massey
- * Date: 15/08/12
+ * User: rowan.massey Date: 15/08/12
  */
 @Entity
 @Cache(usage = CacheConcurrencyStrategy.READ_WRITE)
 @Audited
 @AuditTable(value = "Account_Audit")
-@AttributeOverrides({@AttributeOverride(name = "id", column = @Column(name = "account_id")),
-        @AttributeOverride(name = "version", column = @Column(name = "account_version"))})
+@AttributeOverrides({ @AttributeOverride(name = "id", column = @Column(name = "account_id")),
+		@AttributeOverride(name = "version", column = @Column(name = "account_version")) })
 public class Account extends BaseEntity implements Serializable {
 
-    private String name;
-    private String number;
-    private AccountType type;
-    private double openingBalance;
-    //This field needs to be here, would be too slow if calculating it from all the transactions.
-    private double balance;
-    private Date openingDate;
-    private Currency currency;
-    private Users user;
+	private String name;
+	private String number;
+	private AccountType type;
+	private double openingBalance;
+	//This field needs to be here, would be too slow if calculating it from all the transactions.
+	private double balance;
+	private Date openingDate;
+	private Currency currency;
+	private Users user;
 
+	@NaturalId
+	@Column(name = "account_name", nullable = false)
+	public String getName() {
+		return name;
+	}
 
-    @NaturalId
-    @Column(name = "account_name", nullable = false)
-    public String getName() {
-        return name;
-    }
+	public void setName(String name) {
+		this.name = name;
+	}
 
-    public void setName(String name) {
-        this.name = name;
-    }
+	@NotAudited
+	@ManyToOne(fetch = FetchType.EAGER, cascade = { CascadeType.PERSIST, CascadeType.MERGE })
+	@JoinColumn(name = "account_type_id")
+	public AccountType getType() {
+		return type;
+	}
 
-    @NotAudited
-    @ManyToOne(fetch = FetchType.EAGER, cascade = {CascadeType.PERSIST, CascadeType.MERGE})
-    @JoinColumn(name = "account_type_id")
-    public AccountType getType() {
-        return type;
-    }
+	public void setType(AccountType type) {
+		this.type = type;
+	}
 
-    public void setType(AccountType type) {
-        this.type = type;
-    }
+	@Column(name = "account_openingBalance", nullable = false)
+	public double getOpeningBalance() {
+		return openingBalance;
+	}
 
-    @Column(name = "account_openingBalance", nullable = false)
-    public double getOpeningBalance() {
-        return openingBalance;
-    }
+	public void setOpeningBalance(double balance) {
+		this.openingBalance = balance;
+	}
 
-    public void setOpeningBalance(double balance) {
-        this.openingBalance = balance;
-    }
+	@NotAudited
+	@ManyToOne(fetch = FetchType.EAGER, cascade = { CascadeType.PERSIST, CascadeType.MERGE })
+	@JoinColumn(name = "account_currency_id")
+	public Currency getCurrency() {
+		return currency;
+	}
 
-    @NotAudited
-    @ManyToOne(fetch = FetchType.EAGER, cascade = {CascadeType.PERSIST, CascadeType.MERGE})
-    @JoinColumn(name = "account_currency_id")
-    public Currency getCurrency() {
-        return currency;
-    }
+	public void setCurrency(Currency currency) {
+		this.currency = currency;
+	}
 
-    public void setCurrency(Currency currency) {
-        this.currency = currency;
-    }
+	@Column(name = "account_openingDate", nullable = false)
+	public Date getOpeningDate() {
+		return openingDate;
+	}
 
-    @Column(name = "account_openingDate", nullable = false)
-    public Date getOpeningDate() {
-        return openingDate;
-    }
+	public void setOpeningDate(Date openingDate) {
+		this.openingDate = openingDate;
+	}
 
-    public void setOpeningDate(Date openingDate) {
-        this.openingDate = openingDate;
-    }
+	@Column(name = "account_number", nullable = false)
+	public String getNumber() {
+		return number;
+	}
 
-    @Column(name = "account_number", nullable = false)
-    public String getNumber() {
-        return number;
-    }
+	public void setNumber(String number) {
+		this.number = number;
+	}
 
-    public void setNumber(String number) {
-        this.number = number;
-    }
+	@Column(name = "account_balance", nullable = false)
+	public double getBalance() {
+		return balance;
+	}
 
-    @Column(name = "account_balance", nullable = false)
-    public double getBalance() {
-        return balance;
-    }
+	public void setBalance(double balance) {
+		this.balance = balance;
+	}
 
-    public void setBalance(double balance) {
-        this.balance = balance;
-    }
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "account_user_id", nullable = false)
+	public Users getUser() {
+		return user;
+	}
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "account_user_id", nullable = false)
-    public Users getUser() {
-        return user;
-    }
-
-    public void setUser(Users user) {
-        this.user = user;
-    }
+	public void setUser(Users user) {
+		this.user = user;
+	}
 }
-
---
+++ web/src/main/resources/static/functionality/accounts/account-service-test.js
@@ -0,0 +1,257 @@
+/**
+ * This file tests all of the user service functionality.
+ *
+ * User: rowan.massey
+ * Date: 17/03/2015
+ * Time: 16:14
+ */
+
+describe('Service: mnyAccountService', function () {
+    beforeEach(module('moneyApp'));
+
+    var $injector,
+        $httpBackend,
+        $q,
+        deferred,
+        mnyAccountService,
+        account,
+        result;
+
+
+    beforeEach(inject(function (_$injector_, _$httpBackend_, _$q_) {
+        $injector = _$injector_;
+        $httpBackend = _$httpBackend_;
+        $q = _$q_;
+
+        deferred = $q.defer();
+
+        mnyAccountService = $injector.get("mnyAccountService");
+
+        account = {
+            "name": "Current",
+            "number": "12345",
+            "balance": "2000.00",
+            "openingDate": "2015-03-17T18:49:41Z"
+        };
+    }));
+
+    it('Should save a new account', function () {
+        $httpBackend.whenPOST("api/accounts").respond({
+            data: {
+                headers: function () {
+                    return {
+                        location: "http://localhost/account/1",
+                        expires: 0
+                    };
+                }
+            }
+        });
+        mnyAccountService.save(account).then(function (data) {
+            result = data;
+        });
+        $httpBackend.flush();
+
+        expect(result).toBeDefined();
+        expect(result.data.data.headers().location).toBe("http://localhost/account/1");
+    });
+
+    it('Should respond with an error when saving account.', function () {
+        $httpBackend.whenPOST("api/accounts").respond(500, '');
+        expect(function () {
+            mnyAccountService.save(account);
+            $httpBackend.flush();
+        }).toThrow();
+    });
+
+    it('Should remove an existing account', function () {
+        $httpBackend.whenDELETE("api/accounts/1").respond({
+            data: {
+                status: "201"
+            }
+        });
+
+        mnyAccountService.remove("api/accounts/1").then(function (data) {
+            result = data;
+        });
+        $httpBackend.flush();
+
+        expect(result).toBeDefined();
+        expect(result.config.method).toBe("DELETE");
+        expect(result.status).toBe(200);
+    });
+
+    it('Should respond with an error when deleting an account.', function () {
+        $httpBackend.whenDELETE("api/accounts/1").respond(500, '');
+        expect(function () {
+            mnyAccountService.remove("api/accounts/1");
+            $httpBackend.flush();
+        }).toThrow();
+    });
+
+    it('Should get all accounts.', function () {
+        $httpBackend.whenGET("api/accounts").respond({
+            "_links": {
+                "self": {
+                    "href": "http://localhost:8080/api/accounts{?page,size,sort}",
+                    "templated": true
+                },
+                "search": {
+                    "href": "http://localhost:8080/api/accounts/search"
+                }
+            },
+            "_embedded": {
+                "accounts": [{
+                    "name": "Current",
+                    "number": "12345",
+                    "balance": "2000.00",
+                    "openingDate": "2015-03-17T18:49:41Z",
+                    "_links": {
+                        "self": {
+                            "href": "http://localhost:8080/api/accounts/1"
+                        },
+                        "users": {
+                            "href": "http://localhost:8080/api/accounts/1/users"
+                        }
+                    }
+                },
+                    {
+                        "name": "Wallet",
+                        "number": "1234",
+                        "balance": "50.00",
+                        "openingDate": "2015-03-17T18:49:41Z",
+                        "_links": {
+                            "self": {
+                                "href": "http://localhost:8080/api/accounts/2"
+                            },
+                            "users": {
+                                "href": "http://localhost:8080/api/accounts/2/users"
+                            }
+                        }
+                    }]
+            },
+            "page": {
+                "size": 20,
+                "totalElements": 2,
+                "totalPages": 1,
+                "number": 0
+            }
+        });
+
+        mnyAccountService.getAllAccounts().then(function (data) {
+            result = data;
+        });
+        $httpBackend.flush();
+
+        expect(result).toBeDefined();
+        expect(result.length).toBeGreaterThan(1);
+        expect(result[0].name).toBe("Current");
+        expect(result[0].number).toBe("12345");
+        expect(result[0].balance).toBe("2000.00");
+        expect(result[0]._links.users.href).toBe("http://localhost:8080/api/accounts/1/users");
+
+        expect(result[1].name).toBe("Wallet");
+        expect(result[1].number).toBe("1234");
+        expect(result[1].balance).toBe("50.00");
+        expect(result[1]._links.users.href).toBe("http://localhost:8080/api/accounts/2/users");
+    });
+
+    it('Should resolve to blank when there is no payload in _embedded.', function () {
+        $httpBackend.whenGET("api/accounts").respond({
+            "_links": {
+                "self": {
+                    "href": "http://localhost:8080/api/accounts{?page,size,sort}",
+                    "templated": true
+                },
+                "search": {
+                    "href": "http://localhost:8080/api/accounts/search"
+                }
+            },
+            "page": {
+                "size": 20,
+                "totalElements": 2,
+                "totalPages": 1,
+                "number": 0
+            }
+        });
+        mnyAccountService.getAllAccounts().then(function (data) {
+            result = data;
+        });
+        $httpBackend.flush();
+        expect(result).toBeDefined();
+        expect(result._links).toBeDefined();
+        expect(result._embedded).toBeUndefined();
+    });
+
+    it('Should respond with an error when getting all accounts.', function () {
+        $httpBackend.whenGET("api/accounts").respond(500, '');
+        expect(function () {
+            mnyAccountService.getAllAccounts("api/users");
+            $httpBackend.flush();
+        }).toThrow();
+    });
+
+    it('Should get an account.', function () {
+        $httpBackend.whenGET("api/accounts/2").respond({
+            "name": "Wallet",
+            "number": "1234",
+            "balance": "50.00",
+            "openingDate": "2015-03-17T18:49:41Z",
+            "_links": {
+                "self": {
+                    "href": "http://localhost:8080/api/accounts/2"
+                },
+                "users": {
+                    "href": "http://localhost:8080/api/accounts/2/users"
+                }
+            }
+        });
+        mnyAccountService.getAccount("api/accounts/2").then(function (data) {
+            result = data;
+        });
+        $httpBackend.flush();
+
+        expect(result).toBeDefined();
+        expect(result.name).toBe("Wallet");
+        expect(result.number).toBe("1234");
+        expect(result.balance).toBe("50.00");
+        expect(result._links.users.href).toBe("http://localhost:8080/api/accounts/2/users");
+    });
+
+    it('Should respond with an error when getting account.', function () {
+        $httpBackend.whenGET("api/accounts/2").respond(500, '');
+        expect(function () {
+            mnyAccountService.getAccount("api/accounts/2");
+            $httpBackend.flush();
+        }).toThrow();
+    });
+
+    it('Should update an existing account.', function () {
+        $httpBackend.whenPUT("api/accounts/1").respond({
+            data: {
+                headers: function () {
+                    return {
+                        location: "http://localhost/api/accounts/1",
+                        expires: 0
+                    };
+                }
+            }
+        });
+        mnyAccountService.updateAccount("api/accounts/1", account).then(function (data) {
+            result = data;
+        });
+        $httpBackend.flush();
+
+        expect(result).toBeDefined();
+        expect(result.data.headers().location).toBe("http://localhost/api/accounts/1");
+    });
+
+    it('Should respond with an error when updating an account.', function () {
+        $httpBackend.whenPUT("api/account/1").respond(500, '');
+        expect(function () {
+            mnyAccountService.updateAccount("api/account/1");
+            $httpBackend.flush();
+        }).toThrow();
+    });
+
+});
+
